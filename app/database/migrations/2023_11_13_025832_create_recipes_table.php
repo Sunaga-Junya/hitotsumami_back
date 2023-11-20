02476da0e818cfb6df7e3906892865d0fdcc2a45
須長 淳也 <j_sunaga@MacBook-Pro.local>
2023-11-20 18:54:50 +0900

    add: migration file create table

---
+++ app/database/migrations/2023_11_13_025832_create_recipes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained();
+            $table->string("name");
+            $table->text("description");
+            $table->integer("time_required_min");
+            $table->json("seasonings");
+            $table->json("steps");
+            $table->string("image_path");
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp("updated_at")->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
