0a70d1620de35d9278ef3df3605e69d041c6940b
須長 淳也 <j_sunaga@MacBook-Pro.local>
2023-12-04 15:46:35 +0900

    add: recipe router

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\recipes\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(RecipeController::class)->group(function () {
+        Route::post('/recipes', 'post');
+    });
+});
+
+Route::get('/check', function () {
+    return 'json';
 });
